9a800084938f6c3e4646cb53eb54fe57b676d46b
FallenCap <fallencap18@gmail.com>
2023-08-07 21:48:32 +0530

    2nd Commit

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './Models/schema.js';
+import { games, authors, reviews } from './_db.js';
+
+const resolvers = {
+  // Resolver function.
+  Query: {
+    games() {
+      return games;
+    },
+    reviews() {
+      return reviews;
+    },
+    authors() {
+      return authors;
+    },
+  },
+};
 
 //TODO:  server setup
 
 const port = 4000;
 const server = new ApolloServer({
-    //* typeDefs -- Definations of types of data.
-    //* resolvers
+  typeDefs, //* typeDefs -- Definations of types of data.
+  resolvers, //* resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
