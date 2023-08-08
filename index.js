f49575b56ac704c02c5bd16eb5cc33147ec75b77
FallenCap <fallencap18@gmail.com>
2023-08-08 21:05:10 +0530

    3rd Commit

--- index.js
+++ index.js
@@ -15,6 +15,55 @@ const resolvers = {
     authors() {
       return authors;
     },
+    review(_, args) {
+      return reviews.find((review) => review.id === args.id);
+    },
+    game(_, args) {
+      return games.find((game) => game.id === args.id);
+    },
+    author(_, args) {
+      return authors.find((author) => author.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return reviews.filter((r) => r.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return reviews.filter((r) => r.author_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return authors.find((a) => a.id === parent.author_id);
+    },
+    game(parent) {
+      return games.find((g) => g.id === parent.game_id);
+    },
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      return games.filter((g) => g.id !== args.id);
+    },
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 1000).toString(),
+      };
+      games.push(game);
+      return game;
+    },
+    updateGame(_, args) {
+      games.map((g) => {
+        if (g.id === args.id) {
+          return { ...g, ...args.edit };
+        }
+        return g;
+      });
+      return games.find((g) => g.id === args.id);
+    },
   },
 };
 
